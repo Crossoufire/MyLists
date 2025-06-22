25e2c607f275eab815a8ee6e045994e30284688e
Crossoufire <47953962+Crossoufire@users.noreply.github.com>
2025-06-22 21:24:04 +0200

    added seed achs

--- src/lib/server/domain/media/tv/anime/achievements.seed.ts
+++ src/lib/server/domain/media/tv/anime/achievements.seed.ts
@@ -1,18 +1,119 @@
-import {MediaType} from "@/lib/server/utils/enums";
+import {AchievementDifficulty, MediaType} from "@/lib/server/utils/enums";
 
 
 export const animeAchievements = [
     {
-        codeName: "completed_anime",
-        name: "Cinephile Marathoner",
-        description: "Awarded for completing anime, because real life has too few explosions and car chases.",
+        codeName: 'completed_anime',
+        name: 'Binge No Jutsu!',
+        description: "Awarded for completing anime, because who needs sleep when there's just one more episode?",
         mediaType: MediaType.ANIME,
         tiers: [
-            { criteria: { count: 100 }, difficulty: 1 },
-            { criteria: { count: 400 }, difficulty: 2 },
-            { criteria: { count: 800 }, difficulty: 3 },
-            { criteria: { count: 1500 }, difficulty: 4 },
-        ]
+            { criteria: { count: 20 }, difficulty: AchievementDifficulty.BRONZE },
+            { criteria: { count: 50 }, difficulty: AchievementDifficulty.SILVER },
+            { criteria: { count: 120 }, difficulty: AchievementDifficulty.GOLD },
+            { criteria: { count: 200 }, difficulty: AchievementDifficulty.PLATINUM },
+        ],
+    },
+    {
+        codeName: 'rated_anime',
+        name: 'Rate My Waifu',
+        description: 'Awarded for rating anime, because judging anime is serious business...',
+        mediaType: MediaType.ANIME,
+        tiers: [
+            { criteria: { count: 10 }, difficulty: AchievementDifficulty.BRONZE },
+            { criteria: { count: 30 }, difficulty: AchievementDifficulty.SILVER },
+            { criteria: { count: 50 }, difficulty: AchievementDifficulty.GOLD },
+            { criteria: { count: 100 }, difficulty: AchievementDifficulty.PLATINUM },
+        ],
+    },
+    {
+        codeName: 'comment_anime',
+        name: 'Weeb-splainer',
+        description: 'Awarded for commenting anime, because sometimes a 3-paragraph rant about plot holes is necessary.',
+        mediaType: MediaType.ANIME,
+        tiers: [
+            { criteria: { count: 10 }, difficulty: AchievementDifficulty.BRONZE },
+            { criteria: { count: 20 }, difficulty: AchievementDifficulty.SILVER },
+            { criteria: { count: 30 }, difficulty: AchievementDifficulty.GOLD },
+            { criteria: { count: 50 }, difficulty: AchievementDifficulty.PLATINUM },
+        ],
+    },
+    {
+        codeName: 'short_anime',
+        name: 'Short King',
+        description: "Awarded for watching anime with less than 20 episodes, because this anime will never have another season!",
+        mediaType: MediaType.ANIME,
+        value: 20,
+        tiers: [
+            { criteria: { count: 5 }, difficulty: AchievementDifficulty.BRONZE },
+            { criteria: { count: 8 }, difficulty: AchievementDifficulty.SILVER },
+            { criteria: { count: 12 }, difficulty: AchievementDifficulty.GOLD },
+            { criteria: { count: 15 }, difficulty: AchievementDifficulty.PLATINUM },
+        ],
+    },
+    {
+        codeName: 'long_anime',
+        name: 'Filler Arc Survivor',
+        description: 'Awarded for watching anime with more than 200 episodes, because you powered through the 50 flashbacks and 20 beach episodes.',
+        mediaType: MediaType.ANIME,
+        value: 200,
+        tiers: [
+            { criteria: { count: 3 }, difficulty: AchievementDifficulty.BRONZE },
+            { criteria: { count: 5 }, difficulty: AchievementDifficulty.SILVER },
+            { criteria: { count: 7 }, difficulty: AchievementDifficulty.GOLD },
+            { criteria: { count: 9 }, difficulty: AchievementDifficulty.PLATINUM },
+        ],
+    },
+    {
+        codeName: 'shonen_anime',
+        name: 'Shonen Showdown!',
+        description:
+            "Awarded for watching Shonen anime, because power-ups and friendship speeches never get old.",
+        mediaType: MediaType.ANIME,
+        value: 'Shounen',
+        tiers: [
+            { criteria: { count: 7 }, difficulty: AchievementDifficulty.BRONZE },
+            { criteria: { count: 12 }, difficulty: AchievementDifficulty.SILVER },
+            { criteria: { count: 17 }, difficulty: AchievementDifficulty.GOLD },
+            { criteria: { count: 22 }, difficulty: AchievementDifficulty.PLATINUM },
+        ],
+    },
+    {
+        codeName: 'seinen_anime',
+        name: 'Seinen Sage!',
+        description: "Awarded for completing Seinen anime, because you're too deep into complex plots to enjoy slice-of-life fluff.",
+        mediaType: MediaType.ANIME,
+        value: 'Seinen',
+        tiers: [
+            { criteria: { count: 3 }, difficulty: AchievementDifficulty.BRONZE },
+            { criteria: { count: 5 }, difficulty: AchievementDifficulty.SILVER },
+            { criteria: { count: 8 }, difficulty: AchievementDifficulty.GOLD },
+            { criteria: { count: 12 }, difficulty: AchievementDifficulty.PLATINUM },
+        ],
+    },
+    {
+        codeName: 'network_anime',
+        name: 'Network Lurker',
+        description: 'Awarded for watching anime from different networks, because consistency is key, or maybe your subscription just auto-renewed.',
+        mediaType: MediaType.ANIME,
+        tiers: [
+            { criteria: { count: 8 }, difficulty: AchievementDifficulty.BRONZE },
+            { criteria: { count: 16 }, difficulty: AchievementDifficulty.SILVER },
+            { criteria: { count: 20 }, difficulty: AchievementDifficulty.GOLD },
+            { criteria: { count: 30 }, difficulty: AchievementDifficulty.PLATINUM },
+        ],
+    },
+    {
+        codeName: 'actor_anime',
+        name: 'Seiyuu Sensei',
+        description: 'Awarded for watching anime featuring the same voice actor, because now you can spot that voice faster than an anime power-up scream.',
+        mediaType: MediaType.ANIME,
+        tiers: [
+            { criteria: { count: 8 }, difficulty: AchievementDifficulty.BRONZE },
+            { criteria: { count: 12 }, difficulty: AchievementDifficulty.SILVER },
+            { criteria: { count: 15 }, difficulty: AchievementDifficulty.GOLD },
+            { criteria: { count: 22 }, difficulty: AchievementDifficulty.PLATINUM },
+        ],
     },
 ] as const;
 
--- src/lib/server/domain/media/tv/series/achievements.seed.ts
+++ src/lib/server/domain/media/tv/series/achievements.seed.ts
@@ -1,18 +1,94 @@
-import {MediaType} from "@/lib/server/utils/enums";
+import {AchievementDifficulty, MediaType} from "@/lib/server/utils/enums";
 
 
 export const seriesAchievements = [
     {
-        codeName: "completed_series",
-        name: "Cinephile Marathoner",
-        description: "Awarded for completing series, because real life has too few explosions and car chases.",
-        mediaType: MediaType.ANIME,
+        codeName: 'completed_series',
+        name: 'Couch Potato',
+        description: 'Awarded for completing series, because finishing what you started is a true feat!',
+        mediaType: MediaType.SERIES,
         tiers: [
-            { criteria: { count: 100 }, difficulty: 1 },
-            { criteria: { count: 400 }, difficulty: 2 },
-            { criteria: { count: 800 }, difficulty: 3 },
-            { criteria: { count: 1500 }, difficulty: 4 },
-        ]
+            { criteria: { count: 30 }, difficulty: AchievementDifficulty.BRONZE },
+            { criteria: { count: 100 }, difficulty: AchievementDifficulty.SILVER },
+            { criteria: { count: 175 }, difficulty: AchievementDifficulty.GOLD },
+            { criteria: { count: 250 }, difficulty: AchievementDifficulty.PLATINUM },
+        ],
+    },
+    {
+        codeName: 'rated_series',
+        name: 'TV Rater',
+        description: 'Awarded for rating series, sharing your opinions with the world.',
+        mediaType: MediaType.SERIES,
+        tiers: [
+            { criteria: { count: 20 }, difficulty: AchievementDifficulty.BRONZE },
+            { criteria: { count: 50 }, difficulty: AchievementDifficulty.SILVER },
+            { criteria: { count: 100 }, difficulty: AchievementDifficulty.GOLD },
+            { criteria: { count: 150 }, difficulty: AchievementDifficulty.PLATINUM },
+        ],
+    },
+    {
+        codeName: 'short_series',
+        name: 'Episode Economist',
+        description: 'Awarded for completing series with less than 8 episodes, proving than good things come in small packages!',
+        mediaType: MediaType.SERIES,
+        value: 8,
+        tiers: [
+            { criteria: { count: 5 }, difficulty: AchievementDifficulty.BRONZE },
+            { criteria: { count: 15 }, difficulty: AchievementDifficulty.SILVER },
+            { criteria: { count: 30 }, difficulty: AchievementDifficulty.GOLD },
+            { criteria: { count: 50 }, difficulty: AchievementDifficulty.PLATINUM },
+        ],
+    },
+    {
+        codeName: 'long_series',
+        name: 'Marathon Maverick',
+        description: 'Awarded for completing series with over 150 episodes, because who needs sleep anyway?',
+        mediaType: MediaType.SERIES,
+        value: 150,
+        tiers: [
+            { criteria: { count: 1 }, difficulty: AchievementDifficulty.BRONZE },
+            { criteria: { count: 3 }, difficulty: AchievementDifficulty.SILVER },
+            { criteria: { count: 7 }, difficulty: AchievementDifficulty.GOLD },
+            { criteria: { count: 15 }, difficulty: AchievementDifficulty.PLATINUM },
+        ],
+    },
+    {
+        codeName: 'comedy_series',
+        name: 'Laugh Track Legend',
+        description: 'Awarded for completing comedy series, because laughter is the best medicine!',
+        mediaType: MediaType.SERIES,
+        value: 'Comedy',
+        tiers: [
+            { criteria: { count: 10 }, difficulty: AchievementDifficulty.BRONZE },
+            { criteria: { count: 20 }, difficulty: AchievementDifficulty.SILVER },
+            { criteria: { count: 40 }, difficulty: AchievementDifficulty.GOLD },
+            { criteria: { count: 70 }, difficulty: AchievementDifficulty.PLATINUM },
+        ],
+    },
+    {
+        codeName: 'drama_series',
+        name: 'Drama Queen',
+        description: 'Awarded for completing drama series, embracing the emotional roller-coaster!',
+        mediaType: MediaType.SERIES,
+        value: 'Drama',
+        tiers: [
+            { criteria: { count: 15 }, difficulty: AchievementDifficulty.BRONZE },
+            { criteria: { count: 35 }, difficulty: AchievementDifficulty.SILVER },
+            { criteria: { count: 60 }, difficulty: AchievementDifficulty.GOLD },
+            { criteria: { count: 80 }, difficulty: AchievementDifficulty.PLATINUM },
+        ],
+    },
+    {
+        codeName: 'network_series',
+        name: 'Channel Surfer',
+        description: 'Awarded for watching series from different networks, mastering the remote like a pro!',
+        mediaType: MediaType.SERIES,
+        tiers: [
+            { criteria: { count: 15 }, difficulty: AchievementDifficulty.BRONZE },
+            { criteria: { count: 25 }, difficulty: AchievementDifficulty.SILVER },
+            { criteria: { count: 40 }, difficulty: AchievementDifficulty.GOLD },
+            { criteria: { count: 60 }, difficulty: AchievementDifficulty.PLATINUM },
+        ],
     },
 ] as const;
 
