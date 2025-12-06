13df940ff89817431cabe492326c5c8a2a50c726
Crossoufire <47953962+Crossoufire@users.noreply.github.com>
2025-12-06 15:50:08 +0100

    Refactor `AppliedFilters` UI
    - Split filters into `normal` and `misc`
    - Improve badges rendering and add grouping
    - Add height and layout improvements

--- src/lib/client/components/media/base/AppliedFilters.tsx
+++ src/lib/client/components/media/base/AppliedFilters.tsx
@@ -1,4 +1,5 @@
 import {X} from "lucide-react";
+import {Fragment} from "react";
 import {MediaType} from "@/lib/utils/enums";
 import {Badge} from "@/lib/client/components/ui/badge";
 import {MediaListArgs} from "@/lib/types/zod.schema.types";
@@ -17,7 +18,10 @@ export const AppliedFilters = ({ totalItems, onFilterRemove }: AppliedFiltersPro
     const { mediaType } = useParams({ from: "/_main/_private/list/$mediaType/$username" });
     const { page: _page, sort: _sort, ...rawFilters } = useSearch({ from: "/_main/_private/list/$mediaType/$username" });
 
+    const booleanKeys = ["favorite", "comment", "hideCommon"];
     const localFilters = rawFilters as Partial<MediaListArgs>;
+    const miscFilters = Object.entries(localFilters).filter(([key]) => booleanKeys.includes(key));
+    const normalFilters = Object.entries(localFilters).filter(([key]) => !booleanKeys.includes(key));
 
     const removeFilter = <K extends keyof MediaListArgs>(filterKey: K, filterValue: any) => {
         onFilterRemove({ [filterKey]: Array.isArray(localFilters[filterKey]) ? [filterValue] : null });
@@ -40,7 +44,7 @@ export const AppliedFilters = ({ totalItems, onFilterRemove }: AppliedFiltersPro
     };
 
     return (
-        <div className="flex flex-wrap items-center gap-2 mb-8">
+        <div className="flex flex-wrap items-center gap-2 mb-8 h-[42px]">
             <MutedText italic={false}>
                 {totalItems} {capitalize(mediaType)}
             </MutedText>
@@ -48,40 +52,99 @@ export const AppliedFilters = ({ totalItems, onFilterRemove }: AppliedFiltersPro
                 <div className="text-muted-foreground ml-2 mr-2">|</div>
             }
             <>
-                {Object.entries(localFilters).map(([key, value]) =>
-                    Array.isArray(value) ?
-                        value.map((item) =>
-                            <Badge key={`${key}-${item}`} className="h-8 px-4 text-sm gap-2" variant="secondary">
-                                {key === "langs" ?
-                                    getLangCountryName(
-                                        item,
-                                        (mediaType === MediaType.SERIES || mediaType === MediaType.ANIME) ? "region" : "language",
-                                    )
-                                    :
-                                    item
-                                }
-                                <div
-                                    role="button"
-                                    className="hover:opacity-80 -mr-1"
-                                    onClick={() => removeFilter(key as keyof MediaListArgs, item)}
+                {normalFilters.map(([key, value]) =>
+                    <div key={key} className="flex items-center flex-wrap gap-1 rounded-md border border-border/30 px-2 py-1
+                     bg-muted/10 shadow-sm">
+                        <MutedText italic={false} className="mr-1 capitalize text-sm font-medium text-muted-foreground">
+                            {key}
+                        </MutedText>
+
+                        <div className="flex items-center flex-wrap gap-1">
+                            {Array.isArray(value) ?
+                                value.map((item, i) =>
+                                    <Fragment key={`${key}-${item}`}>
+                                        <Badge
+                                            variant="secondary"
+                                            className="h-8 px-3 text-sm gap-1 rounded-full border border-border/50
+                                                bg-secondary hover:bg-secondary/90 transition"
+                                        >
+                                            {key === "langs" ? getLangCountryName(
+                                                    item,
+                                                    mediaType === MediaType.SERIES || mediaType === MediaType.ANIME ? "region" : "language"
+                                                )
+                                                : item
+                                            }
+                                            <div
+                                                role="button"
+                                                className="hover:opacity-80 -mr-1"
+                                                onClick={() => removeFilter(key as keyof MediaListArgs, item)}
+                                            >
+                                                <X className="size-4"/>
+                                            </div>
+                                        </Badge>
+                                        {i < value.length - 1 &&
+                                            <span className="text-muted-foreground text-xs font-medium px-1.5">
+                                                    OR
+                                                </span>
+                                        }
+                                    </Fragment>
+                                )
+                                :
+                                <Badge
+                                    variant="secondary"
+                                    className="h-8 px-3 text-sm gap-1 rounded-full border border-border/50 bg-secondary
+                                    hover:bg-secondary/90 transition"
                                 >
-                                    <X className="size-4"/>
-                                </div>
-                            </Badge>
-                        )
-                        :
-                        // TODO: Not typesafe
-                        <Badge key={key} className="h-8 px-4 text-sm gap-2" variant="secondary">
-                            {(key === "hideCommon" && value === true) ? `No common` :
-                                (key === "favorite" && value === true) ? `Favorites` :
-                                    (key === "comment" && value === true) ? `Commented` :
-                                        value as string
+                                    {String(value)}
+                                    <div
+                                        role="button"
+                                        className="hover:opacity-80 -mr-1"
+                                        onClick={() => removeFilter(key as keyof MediaListArgs, value)}
+                                    >
+                                        <X className="size-4"/>
+                                    </div>
+                                </Badge>
                             }
-                            <div role="button" className="hover:opacity-80 -mr-1" onClick={() => removeFilter(key as keyof MediaListArgs, value)}>
-                                <X className="size-4"/>
-                            </div>
-                        </Badge>
+                        </div>
+                    </div>
                 )}
+                {miscFilters.length > 0 &&
+                    <div className="flex items-center flex-wrap gap-1 rounded-md border border-border/30 px-2 py-1 bg-muted/10 shadow-sm">
+                        <MutedText
+                            italic={false}
+                            className="mr-1 capitalize text-sm font-medium text-muted-foreground"
+                        >
+                            misc
+                        </MutedText>
+
+                        <div className="flex items-center flex-wrap gap-1">
+                            {miscFilters.map(([key, value]) => {
+                                if (value !== true) return null;
+
+                                const label = key === "favorite" ? "Favorites" : key === "comment" ? "Commented"
+                                    : key === "hideCommon" ? "No Common" : String(value);
+
+                                return (
+                                    <Badge
+                                        key={key}
+                                        variant="secondary"
+                                        className="h-8 px-3 text-sm gap-1 rounded-full border border-border/50 bg-secondary
+                                        hover:bg-secondary/90 transition"
+                                    >
+                                        {label}
+                                        <div
+                                            role="button"
+                                            className="hover:opacity-80 -mr-1"
+                                            onClick={() => removeFilter(key as keyof MediaListArgs, value)}
+                                        >
+                                            <X className="size-4"/>
+                                        </div>
+                                    </Badge>
+                                );
+                            })}
+                        </div>
+                    </div>
+                }
                 {Object.keys(localFilters).length > 0 &&
                     <div role="button" className="ml-2" onClick={() => removeAllFilters()}>
                         <MutedText italic={false}>
