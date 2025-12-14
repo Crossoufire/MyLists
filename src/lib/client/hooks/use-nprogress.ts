e8ca162e7fbac72b453e4ec73106c156918d136e
Crossoufire <47953962+Crossoufire@users.noreply.github.com>
2025-12-14 14:44:23 +0100

    Refactor `useNProgress` hook
    - Add customizable delays with `pendingMs` & `pendingMinMs`

--- src/lib/client/hooks/use-nprogress.ts
+++ src/lib/client/hooks/use-nprogress.ts
@@ -6,33 +6,62 @@ import {useRouter} from "@tanstack/react-router";
 NProgress.configure({ showSpinner: false, parent: "body" });
 
 
-export const useNProgress = () => {
+interface ProgressOpts {
+    pendingMs?: number;
+    pendingMinMs?: number;
+}
+
+
+export const useNProgress = ({ pendingMs = 80, pendingMinMs = 200 }: ProgressOpts = {}) => {
     const router = useRouter();
 
     useEffect(() => {
-        const handleStart = ({ pathChanged }: { pathChanged: boolean }) => {
-            if (pathChanged) NProgress.start();
-        }
+        let startedAt = 0;
+        let minTimer: ReturnType<typeof setTimeout> | undefined;
+        let showTimer: ReturnType<typeof setTimeout> | undefined;
+
+        const handleStart = () => {
+            startedAt = 0;
+            showTimer = setTimeout(() => {
+                NProgress.start();
+                startedAt = Date.now();
+                minTimer = setTimeout(() => {
+                    minTimer = undefined;
+                    if (startedAt === -1) NProgress.done();
+                }, pendingMinMs);
+            }, pendingMs);
+        };
 
         const handleComplete = () => {
-            NProgress.done();
+            clearTimeout(showTimer);
+            showTimer = undefined;
+
+            if (startedAt === 0) return;
+            if (startedAt === -1) return;
+
+            if (minTimer) {
+                startedAt = -1;
+            }
+            else {
+                NProgress.done();
+            }
         };
 
-        const handleError = () => {
-            NProgress.done();
-        };
+        const handleError = () => handleComplete();
 
-        const unsubscribeError = router.subscribe("onResolved", handleError);
-        const unsubscribeStart = router.subscribe("onBeforeLoad", handleStart);
-        const unsubscribeComplete = router.subscribe("onLoad", handleComplete);
+        const unsubError = router.subscribe("onResolved", handleError);
+        const unsubStart = router.subscribe("onBeforeLoad", handleStart);
+        const unsubComplete = router.subscribe("onLoad", handleComplete);
 
         return () => {
-            unsubscribeStart();
-            unsubscribeComplete();
-            unsubscribeError();
+            unsubStart();
+            unsubComplete();
+            unsubError();
+            clearTimeout(showTimer);
+            clearTimeout(minTimer);
             NProgress.done();
-        }
-    }, [router]);
+        };
+    }, [router, pendingMs, pendingMinMs]);
 
     return null;
 };
