a4896d31363ed9d9f534d033bc1a5490424f6692
Crossoufire <47953962+Crossoufire@users.noreply.github.com>
2025-12-28 01:44:21 +0100

    Update about.tsx

--- src/routes/_main/_universal/about.tsx
+++ src/routes/_main/_universal/about.tsx
@@ -16,59 +16,59 @@ function AboutPage() {
                 I'm only one person (french) maintaining this website. It is just a project on my free time to know
                 where I'm on TV shows, movies, games or books for me and my friends. If you have any constructive
                 remarks, find any bugs or want to be involved in the evolution of MyLists.info, please do not hesitate
-                and&nbsp; <a className="text-blue-600" href={`mailto:${mail}`}>contact me</a>.
+                and&nbsp; <a className="text-app-accent" href={`mailto:${mail}`}>contact me</a>.
             </p>
 
-            <h5 className="mt-7 text-xl font-semibold mb-1">Flask</h5>
+            <h5 className="mt-7 text-xl font-semibold mb-1">Bun / Tanstack Start</h5>
             <p>
                 MyLists.info is powered server side by
-                <a className="text-blue-600" href="https://palletsprojects.com/"> Flask</a>.
+                <a className="text-app-accent" href="https://tanstack.com/start/latest/docs/framework/react/overview"> Tanstack Start</a>.
                 The license of Flask can be found&nbsp;
-                <a className="text-blue-600" href="https://flask.palletsprojects.com/en/latest/license/">here</a>.
+                <a className="text-app-accent" href="https://github.com/TanStack/router?tab=MIT-1-ov-file">here</a>.
             </p>
 
             <h5 className="mt-7 text-xl font-semibold mb-1">React</h5>
             <p>
                 MyLists.info is powered front side by
-                <a className="text-blue-600" href="https://react.dev/"> React</a>.
+                <a className="text-app-accent" href="https://react.dev/"> React</a>.
                 The license of react can be found&nbsp;
-                <a className="text-blue-600" href="https://github.com/facebook/react/blob/main/LICENSE">here</a>.
+                <a className="text-app-accent" href="https://github.com/facebook/react/blob/main/LICENSE">here</a>.
             </p>
 
             <h5 className="mt-7 text-xl font-semibold mb-1">Shadcn-UI</h5>
             <p className="text-justify">
-                MyLists.info uses <a className="text-blue-600" href="https://ui.shadcn.com/">Shadcn/ui</a>.
+                MyLists.info uses <a className="text-app-accent" href="https://ui.shadcn.com/">Shadcn/ui</a>.
                 The license can be found&nbsp;
-                <a className="text-blue-600" href="https://github.com/shadcn-ui/ui/blob/main/LICENSE.md">here</a>.
+                <a className="text-app-accent" href="https://github.com/shadcn-ui/ui/blob/main/LICENSE.md">here</a>.
             </p>
 
             <h5 className="mt-7 text-xl font-semibold mb-1">TMDB API</h5>
             <p>
-                MyLists.info uses the <a className="text-blue-600" href="https://www.themoviedb.org/"> TMDB </a>
+                MyLists.info uses the <a className="text-app-accent" href="https://www.themoviedb.org/"> TMDB </a>
                 API but is not endorsed or certified by TMDB. Here is the link to the
-                <a className="text-blue-600" href="https://www.themoviedb.org/documentation/api"> TMDb API</a>.
+                <a className="text-app-accent" href="https://www.themoviedb.org/documentation/api"> TMDb API</a>.
             </p>
 
             <h5 className="mt-7 text-xl font-semibold mb-1">IGDB API</h5>
             <p>
-                MyLists.info uses the <a className="text-blue-600" href="https://www.igdb.com/"> IGDB </a>
+                MyLists.info uses the <a className="text-app-accent" href="https://www.igdb.com/"> IGDB </a>
                 API but is not endorsed or certified by IGDB. Here is the link to the
-                <a className="text-blue-600" href="https://api-docs.igdb.com/"> IGDB API</a>.
+                <a className="text-app-accent" href="https://api-docs.igdb.com/"> IGDB API</a>.
             </p>
 
             <h5 className="mt-7 text-xl font-semibold mb-1">Google Books API</h5>
             <p>
-                MyLists.info uses the <a className="text-blue-600" href="https://books.google.com/"> Google
+                MyLists.info uses the <a className="text-app-accent" href="https://books.google.com/"> Google
                 Books </a>
                 API but is not endorsed or certified by Google.
                 Here is the link to the
-                <a className="text-blue-600" href="https://developers.google.com/books/"> Google Books API</a>.
+                <a className="text-app-accent" href="https://developers.google.com/books/"> Google Books API</a>.
             </p>
 
             <h5 className="mt-7 text-xl font-semibold mb-1">FreePik</h5>
             <p>
                 The Logo was taken on FreePik and created by&nbsp;
-                <a className="text-blue-600"
+                <a className="text-app-accent"
                    href="https://fr.freepik.com/vecteurs-libre/logo-degrade-colore-initial-vecteur-m_28762027.htm#query=blue%20M
                    %20logo%20png&position=3&from_view=search&track=ais&uuid=911d0224-be46-42ed-8f5d-bce057504422">
                     logturnal
