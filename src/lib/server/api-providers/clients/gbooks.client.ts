bddac802edf50674c5c2be990bb6f04638c66d43
Crossoufire <47953962+Crossoufire@users.noreply.github.com>
2026-01-28 23:21:29 +0100

    fix gbooks error

--- src/lib/server/api-providers/clients/gbooks.client.ts
+++ src/lib/server/api-providers/clients/gbooks.client.ts
@@ -2,6 +2,7 @@ import {RateLimiterAbstract} from "rate-limiter-flexible";
 import {createRateLimiter} from "@/lib/server/core/rate-limiter";
 import {BaseClient} from "@/lib/server/api-providers/clients/base.client";
 import {GBooksDetails, GBooksSearchResults, SearchData} from "@/lib/types/provider.types";
+import {serverEnv} from "@/env/server";
 
 
 export class GBooksClient extends BaseClient {
@@ -21,7 +22,7 @@ export class GBooksClient extends BaseClient {
     async search(query: string, page: number = 1): Promise<SearchData<GBooksSearchResults>> {
         const offset = (page - 1) * this.resultsPerPage;
 
-        const url = `${this.baseUrl}?q=${query}&startIndex=${offset}`;
+        const url = `${this.baseUrl}?q=${query}&startIndex=${offset}&key=${serverEnv.GOOGLE_BOOKS_API_KEY}`;
         const response = await this.call(url);
 
         return {
