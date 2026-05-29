79c4bbcf7e7fdd45a28632f99aa0b8cc13454f45
Crossoufire <vince.delmas.38@gmail.com>
2026-05-29 21:28:37 +0200

    add csrf middleware

--- src/start.ts
+++ src/start.ts
@@ -1,12 +1,15 @@
-import {createStart} from "@tanstack/react-start";
+import {createCsrfMiddleware, createStart} from "@tanstack/react-start";
 import {funcErrorMiddleware, reqErrorMiddleware} from "@/lib/server/middlewares/global-error";
 import {formattedErrorAdapter, formZodErrorAdapter, unauthorizedErrorAdapter} from "@/lib/utils/error-classes";
+
+
+const csrfMiddleware = createCsrfMiddleware({ filter: (ctx) => ctx.handlerType === "serverFn" });
 
 
 export const startInstance = createStart(() => {
     return {
         defaultSsr: false,
-        requestMiddleware: [reqErrorMiddleware],
+        requestMiddleware: [csrfMiddleware, reqErrorMiddleware],
         functionMiddleware: [funcErrorMiddleware],
         serializationAdapters: [
             formZodErrorAdapter,
